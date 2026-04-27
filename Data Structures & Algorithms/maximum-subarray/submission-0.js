923d2c0ba3ebc8d444359c656f7d32da7e044ff4
Muhammad Faizan <mf1341876@gmail.com>
2026-04-27 09:29:08 +0500

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums) {
+        let currSum = nums[0];
+        let maxSum = nums[0];
+
+        for(let i =1; i<nums.length; i++) {
+            currSum = Math.max(currSum+nums[i],nums[i]);
+            maxSum = Math.max(currSum,maxSum);
+        }
+        return maxSum;
+    }
+}
