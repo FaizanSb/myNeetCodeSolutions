8e97d213db768ba52b5f1fd1d7de9aeb3d027f83
Muhammad Faizan <mf1341876@gmail.com>
2026-06-01 11:16:08 +0500

    Add: number-of-one-bits - submission-1

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number}
+     */
+    hammingWeight(n) {
+        let count = 0;
+        for(let i = 31; i>=0; i--) {
+            if((n>>i) & 1 === 1) {
+                count++;
+            }
+            
+        }
+        return count;   
+    }
+}
